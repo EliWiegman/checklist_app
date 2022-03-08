fa0fa0fcf3de577d8205b6a0e9692412fb37a8ca
EliWiegman <ebwiegman@gmail.com>
2022-03-07 20:36:04 -0500

    working on labeledCheckbox

---
+++ lib/globals.dart
@@ -0,0 +1,9 @@
+var items = [];
+const sampleText = "hello world";
+
+const urmom = "urmom";
+const falseVar = false;
+
+int dummy() {
+  return 0;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,182 @@
 import 'package:flutter/material.dart';
+import 'globals.dart' as globals;
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const title = 'Basic List';
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.all(15.0),
+          children: <Widget>[
+            CheckBoxWrapper(),
+            LabeledCheckbox(label: "stuff", value: false, onChanged: dummy)
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+bool dummy() {
+  return true;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CheckBoxWrapper extends StatefulWidget {
+  const CheckBoxWrapper({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  @override
+  State<CheckBoxWrapper> createState() => _CheckBoxWrapperState();
+}
+
+class _CheckBoxWrapperState extends State<CheckBoxWrapper> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
+    return Checkbox(
+      checkColor: Colors.white,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
+    );
+  }
+}
+
+// class LabeledCheckbox extends StatefulWidget {
+//   const LabeledCheckbox(
+//       {Key? key,
+//       this.label = "urmom",
+//       this.value = false,
+//       this.onChanged = globals.dummy()})
+//       : super(key: key);
+
+//   final String label;
+//   final bool value;
+//   final Function onChanged;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onTap: () {
+//         onChanged(!value);
+//       },
+//       child: Row(
+//         children: <Widget>[
+//           Checkbox(
+//             value: value,
+//             onChanged: (bool? newValue) {
+//               onChanged(newValue);
+//             },
+//           ),
+//           Text(label),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class _LabeledCheckboxState extends State<LabeledCheckbox> {
+//   bool _isSelected = false;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return LabeledCheckbox(
+//       label: 'This is the label text',
+//       //padding: const EdgeInsets.symmetric(horizontal: 20.0),
+//       value: _isSelected,
+//       onChanged: (bool newValue) {
+//         setState(() {
+//           _isSelected = newValue;
+//         });
+//       },
+//     );
+//   }
+// }
+// class CheckBoxWrapper extends StatefulWidget {
+//   const CheckBoxWrapper({Key? key}) : super(key: key);
+
+//   @override
+//   State<CheckBoxWrapper> createState() => _CheckBoxWrapperState();
+// }
+
+class LabeledCheckbox extends StatefulWidget {
+  const LabeledCheckbox({
+    Key? key,
+    required this.label,
+    required this.value,
+    required this.onChanged,
+  }) : super(key: key);
+
+  final String label;
+  final bool value;
+  final Function onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onChanged(!value);
+      },
+      child: Row(
+        children: <Widget>[
+          Checkbox(
+            value: value,
+            onChanged: (bool? newValue) {
+              onChanged(newValue);
+            },
+          ),
+          Text(label),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  @override
+  State<LabeledCheckbox> createState() => _LabeledCheckboxState();
+}
+
+class _LabeledCheckboxState extends State<LabeledCheckbox> {
+  bool _isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return LabeledCheckbox(
+      label: 'This is the label text',
+      //padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      value: _isSelected,
+      onChanged: (bool newValue) {
+        setState(() {
+          _isSelected = newValue;
+        });
+      },
     );
   }
 }
